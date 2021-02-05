987fe3310738bc49dc6a78f167fca74f17e35703
mazher1998 <62439236+mazher1998@users.noreply.github.com>
2021-02-05 12:42:52 +0500

    Add files via upload

--- src/component/homeSection/HeroSection.jsx
+++ src/component/homeSection/HeroSection.jsx
@@ -9,7 +9,7 @@ import { Link } from "react-scroll";
         <div className=  'homecontainer'>
             
             <div >
-            <h1 className=  'abc'>Welcome to MontageArtMedia</h1>
+            <h1 >Welcome to MontageArtMedia</h1>
             <p1 className=  'ac'>Montage aims to deliver creative solutions with excellence. We are a full-service production agency developing bespoke</p1>
             <br/> <p1 className=  'ac'>creative strategies for brands, businesses, art galleries and individuals. In this digital age Content is the King, it</p1>
             <br/>   <p1 className=  'ac'>has a seminal role in unleashing the power of storytelling, a tool that can contribute to inclusion and connection, </p1>
