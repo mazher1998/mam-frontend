eac258dfcfb4c3ac576672087b328aedbfbc09a7
mazher1998 <62439236+mazher1998@users.noreply.github.com>
2021-02-05 12:09:28 +0500

    Delete src directory

--- src/axios-server.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-const instance=axios.create({
-    baseURL:"http://montageartmedia.herokuapp.com"
-})
-export default instance;
--- src/store/action/types.js
+++
@@ -1,4 +0,0 @@
-export const LOGIN_SUCCESS='LOGIN_SUCCESS';
-export const LOGOUT='LOGOUT';
-export const GET_REVIEWS = 'GET_REVIEWS';
-export const GET_USERS = 'GET_USERS';
